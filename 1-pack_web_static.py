71ee8139aec5215f1297676b20a1df09bcff64ae
Innocent_Bern <innocentbern22@gmail.com>
2023-10-06 00:41:21 +0300

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Fabric script that compress contents of web_static dir"""
+
+
+import os
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """Generates a.tgz archive"""
+
+    if os.path.isdir('./versions') is False:
+        os.mkdir('./versions')
+        print("Made dir")
+
+    cur = datetime.utcnow()
+    fname = "web_static_{}{}{}{}{}{}.tgz".format(cur.year,
+                                                 cur.month,
+                                                 cur.day,
+                                                 cur.hour,
+                                                 cur.minute,
+                                                 cur.second)
+    local("tar czf {}  ./web_static".format(fname))
+    local("mv {} ./versions".format(fname))
+    return "versions/{}".format(fname)
